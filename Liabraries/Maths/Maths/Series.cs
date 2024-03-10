9d9f04e7910dd90d3ac32d84ccd189311b56b87e
baharerol <baharerol.79@gmail.com>
2024-03-10 14:19:55 +0300

    [Feature] Week_1 has been added.

---
+++ Liabraries/Maths/Maths/Series.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maths
+{
+    public class Series<T>
+    {
+        private T[] _series;
+
+        public T FirstItem
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public T LastItem
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Count => throw new NotImplementedException();
+
+        public Series(T[] inputs)
+        {
+            _series = inputs;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder s = new StringBuilder();
+            foreach (var item in _series)
+            {
+                s.Append(item);
+            }
+            return s.ToString();
+        }
+
+    }
+}
---
+++ Tests/MathsTests/NumberTests.cs
@@ -0,0 +1,69 @@
+﻿using Maths;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MathsTests
+{
+    public class NumberTests
+    {
+        [Fact]
+        public void Check_Multiply()
+        {
+            // Arrange
+            var number = new Number(5);
+
+            // Act
+            var result = number.Multiply();
+
+            // Assert
+            Assert.Equal(10, result);
+        }
+
+
+        [Fact]
+        public void Check_Multiply_With_Parameter()
+        {
+            // Arrange
+            var number = new Number(10);
+
+            // Act
+            var result = number.Value;
+
+            // Assert 
+            Assert.True(true);
+            Assert.False(false);
+            Assert.Equal(5, 10);
+        }
+
+        [Fact]
+        public void Check_Multiply_With_NoParameter()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Fact]
+        public void Check_Number_MinValue()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Fact]
+        public void Check_Number_MaxValue()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Fact]
+        public void Check_ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
