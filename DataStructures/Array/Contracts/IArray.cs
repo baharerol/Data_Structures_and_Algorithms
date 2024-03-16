0512d056bd39c47369102d04cb7c3e63e0a8b686
baharerol <baharerol.79@gmail.com>
2024-03-16 23:00:56 +0300

    [Feature] IArray interface defined.

---
+++ DataStructures/Array/Contracts/IArray.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array.Contracts
+{
+    public interface IArray<T>
+    {
+        int Length { get; }
+        T GetItem(int index);
+        void SetItem(int index, T value);
+    }
+}
