fb96f07bb5a67f0e0f2fd77482c102269317f83a
baharerol <baharerol.79@gmail.com>
2024-03-25 11:57:42 +0300

    [Feature] LinkedList project created.

---
+++ DataStructures/LinkedList/Contracts/ISinglyLinkedList.cs
@@ -0,0 +1,23 @@
+﻿using LinkedList.Singly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList.Contracts
+{
+    public interface ISinglyLinkedList<T>
+    {
+        SinglyLinkedListNode<T>? Head { get; }
+        int Count { get; }
+        void AddFirst(T item);
+        void AddLast(T item);
+        void AddBefore(SinglyLinkedListNode<T> node, T item);
+        void AddAfter(SinglyLinkedListNode<T> node, T item);
+        T RemoveFirst();
+        public T RemoveLast();
+        T Remove(SinglyLinkedListNode<T> node);
+
+    }
+}
