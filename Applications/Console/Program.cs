e348082b09d789a5e1cbe76c451d85dcb28a6240
baharerol <baharerol.79@gmail.com>
2024-04-01 18:48:21 +0300

    [Feature] Console project organized.

--- Applications/Console/Program.cs
+++ Applications/Console/Program.cs
@@ -1,21 +1,3 @@
-﻿using LinkedList.Singly;
+﻿using System;
 
-
-var linkedlist = new SinglyLinkedList<String>();
-linkedlist.AddFirst("Esat");
-linkedlist.AddFirst("İrem");
-linkedlist.AddFirst("Rabia");
-linkedlist.AddFirst("Yuşa");
-linkedlist.AddFirst("İsmail");
-
-var c = linkedlist.Head;
-
-while (c != null)
-{
-    System.Console.WriteLine(c);
-    c = c.Next;
-}
-// foreach (var item in linkedlist)
-// {
-//     System.Console.WriteLine(item);
-// }
+System.Console.WriteLine("Hello World");
