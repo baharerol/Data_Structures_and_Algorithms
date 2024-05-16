d88feaa8b28294d47a56da9ad7f892600566e25d
baharerol <baharerol.79@gmail.com>
2024-05-16 18:52:41 +0300

    [Feature]  Stack project has been created.

---
+++ DataStructures/Stack/ArrayStack.cs
@@ -0,0 +1,52 @@
+﻿using Stack.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class ArrayStack<T> : IStack<T>
+    {
+        private List<T> _innerArray;
+        private int LastIndex => Count - 1;
+        public int Count => _innerArray.Count;
+        public ArrayStack()
+        {
+            _innerArray = new List<T>();
+        }
+
+        public ArrayStack(IEnumerable<T> collection) : this()
+        {
+            foreach (var item in collection)
+            {
+                Push(item);
+            }
+        }
+
+
+        public T? Peek()
+        {
+            return Count == 0
+                ? default
+                : _innerArray[LastIndex];
+        }
+
+        public T? Pop()
+        {
+            if (Count == 0)
+            {
+                throw new Exception("Underflow! Empty stack!");
+            }
+            var temp = _innerArray[LastIndex]; // Peek()
+            _innerArray.RemoveAt(LastIndex);
+            return temp;
+        }
+
+        public void Push(T item)
+        {
+            _innerArray.Add(item);
+        }
+    }
+}
